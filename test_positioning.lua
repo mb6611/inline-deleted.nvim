1734ba47e841248742fbcfcce5235995b9ebaafe
Matthew Banaag <mb6611@princeton.edu>
2025-12-06 21:47:23 -0500

    Add test file

---
+++ test_positioning.lua
@@ -0,0 +1,14 @@
+-- Test file for inline-deleted positioning
+local function test1()
+  print("Line 2")
+  print("Line 3 - WILL DELETE THIS")
+  print("Line 4 - AND THIS")
+  print("Line 5 - AND THIS TOO")
+  print("Line 6")
+end
+
+local function test2()
+  print("Line 10")
+end
+
+
